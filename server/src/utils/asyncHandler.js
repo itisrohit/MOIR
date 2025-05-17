03fcdae2c279293b5b0179faaed204bfce313f9d
Rohit <dv6510@gmail.com>
2025-05-17 16:53:43 +0530

    cleaned js files

--- server/src/utils/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        return Promise.resolve(requestHandler(req, res, next)).catch(next);
-    };
-};
-exports.asyncHandler = asyncHandler;
