00a4e9f1ed9b521ac0b3bd159fe9c90db0ad78d1
Rohit <dv6510@gmail.com>
2025-05-18 19:09:04 +0530

    remove middleware, now doing another check

--- client/src/app/v/layout.tsx
+++ client/src/app/v/layout.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 import MainLayout from "@/components/layout/main_layout";
 
 interface VLayoutProps {
   children: React.ReactNode;
 }
 
-export default function VLayout({ children }: VLayoutProps) {
+export default async function VLayout({ children }: VLayoutProps) {
+  const cookieStore = await cookies();
+  const refreshToken = cookieStore.get('refreshToken')?.value;
+  
+  if (!refreshToken) {
+    redirect('/auth');
+  }
+  
   return <MainLayout>{children}</MainLayout>;
 }
